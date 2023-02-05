4f52b090392b034437e7a116c75bf7044fcb6f32
anton-mykhailichenko <Toha24112000@gmail.com>
2023-02-05 15:19:20 +0200

    second commit

--- orders/urls.py
+++ orders/urls.py
@@ -1,8 +1,8 @@
 from django.conf.urls import include
 from django.contrib import admin
 from django.urls import path, re_path
-from landing import views
+from orders import views
 
 urlpatterns = [
-    # re_path(r'^landing123/', views.landing, name='landing'),
+    re_path(r'^basket_adding/', views.basket_adding, name='basket_adding'),
 ]
--- static/static_dev/js/scripts.js
+++ static/static_dev/js/scripts.js
@@ -10,6 +10,66 @@ $(document).ready(function(){
       var submit_btn = $('#submit-btn-product');
       var product_id = submit_btn.data("product-id");
       var product_name = submit_btn.data("product-name");
+      var product_price = submit_btn.data("product-price");
+      var product_img = submit_btn.data("product-img");
       console.log(product_id);
       console.log(product_name);
-})});
+      console.log(product_price);
+      console.log(product_img);
+
+      var data = {};
+      data.product_id = product_id;
+      data.nmb=nmb;
+      var csrf_token = $('#form_buying_product [name="csrfmiddlewaretoken"]').val();
+      data["csrfmiddlewaretoken"] = csrf_token;
+      var url = form.attr("action");
+
+   console.log(data)
+      $.ajax({
+         url: url,
+         type: 'POST',
+         data: data,
+         cache: true,
+         success: function (data) {
+            console.log("OK");
+            console.log(data.products_total_nmb);
+            if (data.products_total_nmb){
+               $('#basket-total-nmb').text('('+data.products_total_nmb+')');
+            }
+         },
+         error: function (){
+            console.log("error")
+         }
+      })
+
+
+      $('.basket-item ul').append('<li>'+product_name+', '+ nmb + 'quantity '+ 'for ' + product_price +'uah    ' +
+          '<a class="delete-item" href="">x</a>'+
+          '</li>');
+})
+   
+   function delclass(){
+      $('.basket-item').removeClass ('invisible');
+   };
+
+   $('.basket-container').on('click ',function (e) {
+      e.preventDefault();
+      delclass();
+   });
+
+   $('.basket-container').on('mouseover ',function (e) {
+      e.preventDefault();
+      delclass();
+   });
+
+   // $('.basket-container').on('mouseout ',function (e) {
+   //    e.preventDefault();
+   //    delclass();
+   // })
+
+   $(document).on('click',('.delete-item'),function (e) {
+      e.preventDefault();
+      $(this).closest('li').remove()
+
+   })
+});
